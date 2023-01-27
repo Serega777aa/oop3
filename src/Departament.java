34df896a04a3605d8d26b5dc58d0bdd975304895
Your Name <serega777aa@mail.ru>
2023-01-27 13:54:33 +0300

    seminar3

---
+++ src/Departament.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Departament {
+    String deptName;
+    String deptWorker;
+
+    ArrayList<Persona> personaInDept = new ArrayList<>();
+    // dep0, dep1, dep2
+
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+}
